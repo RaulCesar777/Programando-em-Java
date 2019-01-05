e977b6cd13f2053179168d47ea8c31628c2c2753
RaulCesar777 <raulzito737@gmail.com>
2019-01-05 20:58:22 -0200

    Update GradeBookTest.java
    JGHJGHJ

--- GradeBookTest.java
+++ GradeBookTest.java
@@ -1,16 +1,20 @@
 /*
 * @Author: RaulCesar777
 * @Date:   2019-01-05 19:49:55
-* @Last Modified by:   Win10
-* @Last Modified time: 2019-01-05 20:38:29
+* @Last Modified by:   RaulCesar777
+* @Last Modified time: 2019-01-05 20:57:32
 */
-
+	import java.util.Scanner;
 public class GradeBookTest {
 
    
     public static void main(String[] args) {
-        GradeBook mygrade = new GradeBook();														/*metodo display menssage */
-        mygrade.displayMessage();
+    	Scanner entrada = new Scanner(System.in);
+        GradeBook mygrade = new GradeBook();
+        System.out.println("entre com o nome do curso");
+        String nomedoCurso = entrada.nextLine();
+        System.out.println();														/*metodo display menssage */
+        mygrade.displayMessage(nomedoCurso);
     }
     
 }
