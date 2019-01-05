799eb7df4a86208af47524cccdfb62c95c660527
RaulCesar777 <raulzito737@gmail.com>
2019-01-05 20:39:28 -0200

    Create GradeBook.java
    rtry

---
+++ GradeBook.java
@@ -0,0 +1,17 @@
+/*
+* @Author: RaulCesar777
+* @Date:   2019-01-05 19:49:11
+* @Last Modified by:   Win10
+* @Last Modified time: 2019-01-05 20:38:29
+*/
+
+
+
+
+public class GradeBook {
+    public void displayMessage(){
+        System.out.println("bem vindo");
+        
+    }
+    
+}
