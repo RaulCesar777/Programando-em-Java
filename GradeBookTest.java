5715423c941785d178352a788d424d51f17dc263
RaulCesar777 <raulzito737@gmail.com>
2019-01-05 21:49:10 -0200

    Update GradeBookTest.java
    sdf

--- GradeBookTest.java
+++ GradeBookTest.java
@@ -2,8 +2,9 @@
 * @Author: RaulCesar777
 * @Date:   2019-01-05 19:49:55
 * @Last Modified by:   RaulCesar777
-* @Last Modified time: 2019-01-05 20:57:32
+* @Last Modified time: 2019-01-05 21:12:42
 */
+
 	import java.util.Scanner;
 public class GradeBookTest {
 
@@ -13,7 +14,8 @@ public class GradeBookTest {
         GradeBook mygrade = new GradeBook();
         System.out.println("entre com o nome do curso");
         String nomedoCurso = entrada.nextLine();
-        System.out.println();														/*metodo display menssage */
+        System.out.println();	
+
         mygrade.displayMessage(nomedoCurso);
     }
     
