b84a1cdf2f5a3cc4ff01679050e022701d897515
RaulCesar777 <raulzito737@gmail.com>
2019-01-05 20:39:51 -0200

    Create GradeBookTest.java
    sadsd

---
+++ GradeBookTest.java
@@ -0,0 +1,16 @@
+/*
+* @Author: RaulCesar777
+* @Date:   2019-01-05 19:49:55
+* @Last Modified by:   Win10
+* @Last Modified time: 2019-01-05 20:38:29
+*/
+
+public class GradeBookTest {
+
+   
+    public static void main(String[] args) {
+        GradeBook mygrade = new GradeBook();														/*metodo display menssage */
+        mygrade.displayMessage();
+    }
+    
+}
