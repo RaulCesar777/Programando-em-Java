d6f0c6fdd7bdfd195ffc738a94d6d1d58ef1eafc
RaulCesar777 <raulzito737@gmail.com>
2019-01-05 20:58:33 -0200

    Update GradeBook.java
    GHJ

--- GradeBook.java
+++ GradeBook.java
@@ -2,16 +2,16 @@
 * @Author: RaulCesar777
 * @Date:   2019-01-05 19:49:11
 * @Last Modified by:   Win10
-* @Last Modified time: 2019-01-05 20:38:29
+* @Last Modified time: 2019-01-05 20:54:35
 */
 
 
 
 
 public class GradeBook {
-    public void displayMessage(){
-        System.out.println("bem vindo");
-        
+    public void displayMessage(String nomedoCurso){
+        System.out.printf("bem vindo  \n %s! \n", nomedoCurso);
+
     }
     
 }
