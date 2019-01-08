8f3d30565e46ea998f12e76b08769267e801323f
RaulCesar777 <raulzito737@gmail.com>
2019-01-08 00:54:01 -0200

    Update Account.java
    arrumando

--- ATM caixa eletronico/Account.java
+++ ATM caixa eletronico/Account.java
@@ -1,8 +1,8 @@
 /**
  * @author Raul Cesar Mulerschat
  * @email raulzito737@gmail.com
- * @create date 2019-01-07 22:15:58
- * @modify date 2019-01-07 22:15:58
+ * @create date 2019-01-08 00:35:03
+ * @modify date 2019-01-08 00:35:03
  * @desc [description]
  */
 public class Account{
@@ -10,14 +10,15 @@ public class Account{
 
     public Account( double initialBalance){
         if (initialBalance > 0.0) {
-            balance= initialBalance;
+            balance = initialBalance;
         }
     } 
     public void credit (double amount){
 
         balance = balance + amount;
     }
-    public double getBalance(){
+    public double getBalance()
+    {
 
         return balance;
     }   
