ae9d3becf24f6f03dffcbef14ff02ae363a652b6
RaulCesar777 <raulzito737@gmail.com>
2019-01-07 22:49:03 -0200

    inicio da classe balanço

--- ATM caixa eletronico/Account.java
+++ ATM caixa eletronico/Account.java
@@ -5,3 +5,20 @@
  * @modify date 2019-01-07 22:15:58
  * @desc [description]
  */
+public class Account{
+    private double balance;
+
+    public Account( double initialBalance){
+        if (initialBalance > 0.0) {
+            balance= initialBalance;
+        }
+    } 
+    public void credit (double amount){
+
+        balance = balance + amount;
+    }
+    public double getBalance(){
+
+        return balance;
+    }   
+}
