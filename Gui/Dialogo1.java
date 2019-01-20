14e2cd1db01553cbcd88c032158a5571d8c616d8
RaulCesar777 <raulzito737@gmail.com>
2019-01-19 23:28:27 -0200

    Caixa de dialogo

---
+++ Gui/Dialogo1.java
@@ -0,0 +1,20 @@
+/**
+ * @author Raul Cesar Mulerschat
+ * @email raulzito737@gmail.com
+ * @create date 2019-01-19 23:27:44
+ * @modify date 2019-01-19 23:27:44
+ * @desc [painel de dialogo]
+ */
+
+
+
+
+
+import javax.swing.JOptionPane;
+
+public class Dialogo1{
+    public static void main( String[] args){
+
+        JOptionPane.showMessageDialog(null, "raul  ");
+    }
+}
