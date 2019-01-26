6857d4e73a3cc67ca2a93310f8484ca7bae84068
RaulCesar777 <raulzito737@gmail.com>
2019-01-25 22:24:12 -0200

    contador for

---
+++ programas simples/contadorFor.java
@@ -0,0 +1,18 @@
+public class contadorFor{
+    public static void main(String[]args){
+
+        for(int cont=1; cont <=10; cont++){
+            System.out.printf("%d ,", cont);
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+}
