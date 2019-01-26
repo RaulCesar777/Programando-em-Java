cbcf570b07315a1d6b92bc910686245ae4729b3a
RaulCesar777 <raulzito737@gmail.com>
2019-01-25 22:43:08 -0200

    Soma com for compacto não recomendo, mas em todo caso é bom saber para ler codigos alheios

--- programas simples/SomaDentroDoFor.java
+++ programas simples/SomaDentroDoFor.java
@@ -2,8 +2,9 @@ public class SomaDentroDoFor{
     public static void main(String[] args) {
         
         int total=0;
-        for(int num=2;num<=20;num +=2){
-            total+=num; //seria mesma coisa que total= total + num;
+        for(int num=2;num<=20;total +=num,num +=2){
+            // seria mesma coisa que total+=num; 
+            //seria mesma coisa que total= total + num;
             System.out.printf("total recebeu %d e num recebeu %d \n", total,num);
 
 
