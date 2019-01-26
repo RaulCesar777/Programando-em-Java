0dee19c31f15b716b0065adbd56d4e3b19bc37c6
RaulCesar777 <raulzito737@gmail.com>
2019-01-25 22:39:05 -0200

    Soma com for para debugger

---
+++ programas simples/SomaDentroDoFor.java
@@ -0,0 +1,18 @@
+public class SomaDentroDoFor{
+    public static void main(String[] args) {
+        
+        int total=0;
+        for(int num=2;num<=20;num +=2){
+            total+=num; //seria mesma coisa que total= total + num;
+            System.out.printf("total recebeu %d e num recebeu %d \n", total,num);
+
+
+        }
+
+
+
+    }
+
+
+
+}
