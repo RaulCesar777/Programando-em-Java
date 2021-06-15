f2d5816f51f868f80e8d03e967539323493b31cc
raulz <raulz@DESKTOP-Q17PTAJ>
2021-06-14 23:30:35 -0300

    14/06/2021

--- TD (Treino Diario)/WhileCounter.java
+++
@@ -1,9 +0,0 @@
-public class WhileCounter{
-    public static void main(String[]args){
-        int contador =0;
-        while(++contador<=10){
-            System.out.printf("%d%n",contador);              
-            // poderia ser ++contador; mas não era o topico de estudo.
-        }
-    }
-}
